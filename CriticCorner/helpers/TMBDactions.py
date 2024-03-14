ca9054c7c19d59c40a9211adb6a5a4815c08903d
2667764n <2667764n@student.gla.ac.uk>
2024-03-14 13:42:56 +0000

    Extremely basic api functionality

---
+++ CriticCorner/helpers/TMBDactions.py
@@ -0,0 +1,38 @@
+from tmdbv3api import Movie, TMDb, Genre
+
+API_KEY = "e359feb309aff2209a6cfea5553838bf"
+
+movie = Movie()
+movie.api_key = API_KEY
+
+genre = Genre()
+genre.api_key = API_KEY
+raw_genre_data = genre.movie_list()["genres"]
+genres = {raw_genre_data[i]["id"]: raw_genre_data[i]["name"] for i in range(len(raw_genre_data))}
+
+def get_movies_by_search(query: str) -> list:
+    """uses the TMBD api to find results related to the query
+
+    Args:
+        query (str): search string entered by user
+
+    Returns:
+        list: list in order of relevance consisting of all relevant movies to the query. Empty if no movies found
+    """
+    related = movie.search(query)
+    
+    if len(related.get("results")) == 0:
+        return []
+    
+    return [mov for mov in related]
+
+def get_genre_by_id(id: int) -> str:
+    if id in genres:
+        return genres[id]
+    else:
+        return "No such genre id"
+
+if __name__ == "__main__":
+    #test script
+    # print(get_movies_by_search("chungus"))
+    print(get_genre_by_id(10000))
